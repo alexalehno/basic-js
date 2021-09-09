f030e954d6835e71599dc57523bbe5d51e8e8be4
alexalehno <alekswakeup@gmail.com>
2021-09-09 16:38:31 +0300

    feat: add extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,21 +1,45 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  let sep = options.separator;
+  let rep = options.repeatTimes;
+  let add = options.addition;
+  let addRep = options.additionRepeatTimes;
+  let addSep = options.additionSeparator;
+
+  !sep ? (sep = "+") : sep;
+  !addSep ? (addSep = "|") : addSep;
+  !rep ? (rep = 1) : rep;
+  !addRep ? (addRep = 1) : addRep;
+
+  function createAdd() {
+    if (String(add) && String(add) !== "undefined") {
+      let res = (add + addSep).repeat(addRep).split("");
+      return res.slice(0, res.length - addSep.length).join("");
+    }
+    return "";
+  }
+
+  function createRepStr() {
+    str += createAdd();
+    let res = (str + sep).repeat(rep).split("");
+    return res.slice(0, res.length - sep.length).join("");
+  }
+
+  return createRepStr();
 }
