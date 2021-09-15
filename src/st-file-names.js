e63f2b7ea738bcc279dc97da729ecaf5fd18fce9
alexalehno <alekswakeup@gmail.com>
2021-09-15 10:40:46 +0300

    refactor: add file-names

--- src/st-file-names.js
+++ src/st-file-names.js
@@ -1,4 +1,6 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {
+  NotImplementedError
+} from '../extensions/index.js';
 
 /**
  * There's a list of file, since two files cannot have equal names,
@@ -15,7 +17,24 @@ import { NotImplementedError } from '../extensions/index.js';
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-export default function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function renameFiles(names) {
+  let res = [];
+
+  for (let i = 0; i < names.length; i++) {
+    if (!res.includes(names[i])) {
+      res.push(names[i])
+
+    } else {
+      let count = 1;
+
+      for (let j = 1; j < i; j++) {
+        if (names[i] === names[j]) {
+          count++
+        }
+      }
+
+      res.push(`${names[i]}(${count})`)
+    }
+  }
+  return res;
 }
